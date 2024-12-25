fae1cb05bb126f6b9cff513c2645e438a637baec
Daniel Østerballe <dao@fibointercon.com>
2024-12-25 14:57:56 +0100

    Added endpoint which serves html

--- main.go
+++ main.go
@@ -3,16 +3,29 @@ package main
 import (
 	httpServer "httpServer/server"
 	"net"
+	"os"
 )
 
 func main() {
 	serv := httpServer.CreateServer("localhost", 80)
 
 	httpServer.AddHandler(serv, "/test", test)
+	httpServer.AddHandler(serv, "/test2", test2)
 
 	httpServer.StartServer(serv)
 }
 
+func test2(conn net.Conn) string {
+	return "banan"
+}
+
 func test(conn net.Conn) string {
-	return "Test"
+	contentBytes, err := os.ReadFile("test.html")
+
+	contentStr := string(contentBytes)
+	if err != nil {
+		return "Error"
+	}
+
+	return contentStr
 }
